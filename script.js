4a3d0061897b16304157b66f7d1193ec7f17712e
ryoup <ryou.2000.0929@gmail.com>
2025-02-04 22:57:36 +0900

    a

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function processImage(file, conversionTable) {
                     const g = data[index + 1];
                     const b = data[index + 2];
 
-                    if (r >= 220 && g <= 100 && b <= 100) {
+                    if (r >= 220 && g <= 80 && b <= 70) {
                         if (minYForX[x] === null) {
                             minYForX[x] = y;
                         }
